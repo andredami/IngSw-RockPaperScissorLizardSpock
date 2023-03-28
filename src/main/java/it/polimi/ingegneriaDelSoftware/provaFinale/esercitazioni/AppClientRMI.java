f6db57912f1de9c36d04751365064fcee4fe3340
Andrea Damiani <1655566+andredami@users.noreply.github.com>
2023-03-28 04:13:28 +0200

    MVC Distributed Implementation - RMI

--- src/main/java/it/polimi/ingegneriaDelSoftware/provaFinale/esercitazioni/AppClientRMI.java
+++ src/main/java/it/polimi/ingegneriaDelSoftware/provaFinale/esercitazioni/AppClientRMI.java
@@ -2,18 +2,21 @@ package it.polimi.ingegneriaDelSoftware.provaFinale.esercitazioni;
 
 import it.polimi.ingegneriaDelSoftware.provaFinale.esercitazioni.distributed.Server;
 import it.polimi.ingegneriaDelSoftware.provaFinale.esercitazioni.distributed.rmi.ClientImpl;
-import it.polimi.ingegneriaDelSoftware.provaFinale.esercitazioni.distributed.rmi.ServerImpl;
 
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class AppClientRMI
 {
-    public static void main( String[] args ) throws RemoteException {
-        Server server = new ServerImpl();
+    public static void main( String[] args ) throws RemoteException, NotBoundException {
+        Registry registry = LocateRegistry.getRegistry();
+        Server server = (Server) registry.lookup("server");
 
         ClientImpl client = new ClientImpl(server);
         client.run();
--- src/main/java/it/polimi/ingegneriaDelSoftware/provaFinale/esercitazioni/distributed/Client.java
+++ src/main/java/it/polimi/ingegneriaDelSoftware/provaFinale/esercitazioni/distributed/Client.java
@@ -3,11 +3,14 @@ package it.polimi.ingegneriaDelSoftware.provaFinale.esercitazioni.distributed;
 import it.polimi.ingegneriaDelSoftware.provaFinale.esercitazioni.model.Turn;
 import it.polimi.ingegneriaDelSoftware.provaFinale.esercitazioni.model.TurnView;
 
-public interface Client {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Client extends Remote {
     /**
      * Notify the client of a model change
      * @param o     The resulting model view
      * @param arg   The causing event
      */
-    void update(TurnView o, Turn.Event arg);
+    void update(TurnView o, Turn.Event arg) throws RemoteException;
 }
